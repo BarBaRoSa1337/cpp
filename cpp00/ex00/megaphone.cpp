165a35221106bac42258a128e30493ae18a3bea8
BarBaRoSa1337 <achakour@1337.ma>
2025-02-06 11:23:38 +0100

    first push

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,14 +5,17 @@ int main(int ac, char **av) {
 
     int i = 1;
     int j = 0;
+    std::string str;
     while (i < ac)
     {
         j = 0;
-        while (av[i][j])
-        {
-            std::cout << (char)toupper(av[i][j]);
-            j++;
-        }
+        // while (av[i][j])
+        // {
+        //     std::cout << (char)toupper(av[i][j]);
+        //     j++;
+        // }
+        str = av[i];
+        std::cout << str << std::endl;
         i++;
     }
     if (ac == 1)
--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -5,12 +5,11 @@
 int main() {
 
     std::string name, f_name, l_name, nick, secret, num;
-    Contact contacts[8];
+    PhoneBook contacts;
     int i = 0;
 
+    std::cout << "welcome to 90s PhoneBook" << std::endl;
     while (1) {
-
-        std::cout << "welcome to 90s PhoneBook" << std::endl;
 
         std::cout << "Enter 'ADD' to add a contact, 'EXIT' to quit, 'SEARCH' for browsing contacts" << std::endl;
         std::getline(std::cin, name);
@@ -19,15 +18,15 @@ int main() {
         if (name == "ADD") {
             while (1) {
 
-                std::cout << "Enter Contact first name" << std::endl;
+                std::cout << "Enter Contact first name : ";
                 std::getline(std::cin, f_name);
-                std::cout << "Enter Contact last name" << std::endl;
+                std::cout << "Enter Contact last name : ";
                 std::getline(std::cin, l_name);
-                std::cout << "Enter Contact nickname name" << std::endl;
+                std::cout << "Enter Contact nickname name : ";
                 std::getline(std::cin, nick);
-                std::cout << "Enter Contact Phone Number name" << std::endl;
+                std::cout << "Enter Contact Phone Number name : ";
                 std::getline(std::cin, num);
-                std::cout << "Enter Contact darkest secret" << std::endl;
+                std::cout << "Enter Contact darkest secret : ";
                 std::getline(std::cin, secret);
 
                 if (f_name.empty() || l_name.empty() || nick.empty() || secret.empty() || num.empty())
@@ -35,10 +34,14 @@ int main() {
                     std::cout << "fields canno't be empty!" << std::endl;
                     break;
                 }
-                contacts[i].addContact(f_name, l_name, nick, num, secret);
-                ++i;
+                contacts.addContact(f_name, l_name, nick, num, secret, i);
+                if (i == 3) i = 0;
+                else ++i;
+                break;
             }
         }
+        if (name == "SEARCH")
+            contacts.search_contacts(i);
     }
     return 0;
 }
