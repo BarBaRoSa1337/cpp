fb02f4218982c18903fb557f84d10ed77152155e
BarBaRoSa1337 <achakour@1337.ma>
2025-02-21 09:40:40 +0100

    working on ex01

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -2,13 +2,14 @@
 #include <string>
 #include "contact.hpp"
 
-void    PhoneBook::addContact()
+void    PhoneBook::add_contacts()
 {
     std::string f_name;
     std::string l_name;
     std::string nick;
     std::string num;
     std::string secret;
+
     std::cout << "Enter first name: ";
     std::getline(std::cin, f_name);
     std::cout << "Enter last name: ";
@@ -23,49 +24,63 @@ void    PhoneBook::addContact()
         std::cout << "All fields must be filled. Contact not saved." << std::endl;
         return;
     }
-    contacts[index].addContact(f_name, l_name, nick, num, secret);
-    index++;
+    sim[index].join_Contact(f_name, l_name, nick, num, secret);
+    if (index == 8)
+    {
+        is_full = 1;
+        index = 0;
+    }
+    else
+        index++;
 }
 
 void    PhoneBook::search_contacts()
 {
-    int i = 0;
-    std::string index;
-    if (index.empty()) {
+    std::string ind;
+    int         tmp = 0;
+    int         i = 0;
+
+    if (index == 0 && is_full == 0) {
         std::cout << "No contacts available." << std::endl;
         return;
     }
-    std::cout << "     index|first name| last name|  nickname" << std::endl;
-    while (i < index) {
-        contacts[i].displayContact(i);
+    if (is_full)
+        tmp = 8;
+    else
+        tmp = index;
+    while (i < tmp)
+    {
+        sim[i].displayContact();
         i++;
     }
     std::cout << "Enter index of contact: ";
-    std::getline(std::cin, index);
-    if (index.size() != 1 || index[0] < '0' || index[0] > '7') {
+    std::getline(std::cin, ind);
+    if (ind.size() != 1 || ind[0] < '0' || ind[0] > '7' || ind[0] - '0' < index)
+    {
         std::cout << "Invalid index." << std::endl;
         return;
     }
-    contacts[index[0] - '0'].search_contacts();
+    sim[ind[0] - '0'].displayContact();
 }
 
-void    PhoneBook::displayContact(int index)
-{
-    contacts[index].displayContact(index);
-}
+void    Contact::displayContact() {
 
-void Contact::displayContact(int index) {
-    std::cout << index << "|";
     std::cout << first_name << "|";
     std::cout << last_name << "|";
     std::cout << nickname << std::endl;
 }
 
-void    Contact::addContact(std::string f_name, std::string l_name, std::string nick, std::string num, std::string secret)
+void    Contact::join_Contact(std::string f_name, std::string l_name, std::string nick, std::string num, std::string secret)
 {
     f_name.size() > 10 ? first_name = f_name.substr(0, 9) + "." : first_name = f_name;
     l_name.size() > 10 ? last_name = l_name.substr(0, 9) + "." : last_name = l_name;
     nick.size() > 10 ? nickname = nick.substr(0, 9) + "." : nickname = nick;
     num.size() > 10 ? phoneNumber = num.substr(0, 9) + "." : phoneNumber = num;
     secret.size() > 10 ? darkest_secret = secret.substr(0, 9) + "." : darkest_secret = secret;
+}
+
+PhoneBook::PhoneBook()
+{
+    index = 0;
+    is_full = 0;
 }
