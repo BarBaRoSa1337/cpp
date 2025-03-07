923872a85ae30d9e79bacd4be1d38fd0566d6d0c
BarBaRoSa1337 <achakour@1337.ma>
2025-03-07 07:46:49 +0000

    test 1

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -1,13 +1,20 @@
 #include "contact.hpp"
 
-void    Contact::display_full_Contact() {
+void Contact::display_full_Contact() {
 
-    f_name.size() > 10 ? first_name.substr(0, 9) + ".";
-    l_name.size() > 10 ? last_name.substr(0, 9) + ".";
-    nick.size() > 10 ? nickname.substr(0, 9) + ".";
-    num.size() > 10 ? phoneNumber.substr(0, 9) + ".";
-    secret.size() > 10 ? darkest_secret.substr(0, 9) + ".";
-
+    std::string f_name = first_name.size() > 10 ? first_name.substr(0, 9) + "." : first_name;
+    std::string l_name = last_name.size() > 10 ? last_name.substr(0, 9) + "." : last_name;
+    std::string nick = nickname.size() > 10 ? nickname.substr(0, 9) + "." : nickname;
+    std::string Number = Number.size() > 10 ? Number.substr(0, 9) + "." : Number;
+    std::string secret = darkest_secret.size() > 10 ? darkest_secret.substr(0, 9) + "." : darkest_secret;
+    
+    std::cout << "First Name" << "|" << "Last Name" << "|" << "Nick Name" 
+        << "|" << "Number" << "|" << "Darkest secret" << std::endl;
+    std::cout << "|" << std::setw(10) << first_name << "|";
+    std::cout << std::setw(10) << l_name << "|";
+    std::cout << std::setw(10) << nick << "|";
+    std::cout << std::setw(10) << Number << "|";
+    std::cout << std::setw(10) << secret << "|" << std::endl;
 }
 
 void    Contact::join_Contact(std::string f_name, std::string l_name, std::string nick,        std::string num, std::string secret) {
@@ -24,8 +31,8 @@ void    Contact::display_Contact() {
 
     std::cout << "First Name :" << first_name << std::endl;
     std::cout << "Last Name :" << last_name << std::endl;
-    std::cout << "nickname :" << nickname << std::endl;
-    std::cout << "Phone number :" << phoneNumber << std::endl;
-    std::cout << "darkest secret :" << darkest_secret << std::endl;
+    std::cout << "Nick Name :" << nickname << std::endl;
+    std::cout << "Phone Number :" << phoneNumber << std::endl;
+    std::cout << "Darkest Secret :" << darkest_secret << std::endl;
 
 }
