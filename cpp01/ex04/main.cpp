aea08c1f0238b9b682b074c1cfe7f828c5e768af
BarBaRoSa1337 <achakour@student.1337.ma>
2025-03-13 18:24:03 +0000

    working on ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,57 +1,64 @@
 #include <iostream>
-// #include <fstream>
+#include <fstream>
 #include <string>
 
-void replaceAndWriteToFile(const std::string& filename, const std::string& s1, const std::string& s2) {
-    if (s1.empty()) {
-        std::cerr << "Error: The search string cannot be empty." << std::endl;
-        return;
-    }
-
-    std::ifstream inputFile(filename);
-    if (!inputFile) {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return;
-    }
-
-    // std::string outputFilename = filename + ".replace";
-    std::ofstream outputFile(filename + ".replace");
-    if (!outputFile) {
-        std::cerr << "Error: Unable to create file " << filename + ".replace" << std::endl;
-        return;
-    }
-
+void FileReplace(std::ifstream &inFile, std::ofstream &outFile, std::string s1, std::string s2) {
     std::string line;
-    while(inputFile)
-	{
-		std::size_t	pos = 0;
-		getline(inputFile, line);
-		while (true)
-		{
-			pos = line.find(s1, pos);
-			if (pos != std::string::npos)
-			{
-				line.erase(pos, s1.length());
-				line.insert(pos, s2);
-			}
-			else
-				break;
-		}
-		outputFile << line ;
-		if (inputFile.eof())
-			break ;
-		outputFile << std::endl;
-	}
-    inputFile.close();
-    outputFile.close();
-    std::cout << "Replacement complete. Output saved in " << outputFilename << std::endl;
+
+    while (inFile)
+    {
+        std::size_t pos = 0;
+        getline(inFile, line);
+        while (1)
+        {
+            pos = line.find(s1, pos);
+            if (pos != std::string::npos) 
+            {
+                line.erase(pos, s1.length());
+                line.insert(pos, s2);
+            }
+            else
+                break;
+        }
+        outFile << line;
+        if (inFile.eof())
+            break;
+        outFile << std::endl;
+    }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
-        return 1;
+int main(int ac, char *av[]) {
+   std::ofstream outFile;
+   std::ifstream inFile;
+
+    if (ac != 4) {
+        std::cerr << "Error: Usage: <program_name> <filename> <string1> <string2>\n";
+        return (1);
     }
-    replaceAndWriteToFile(argv[1], argv[2], argv[3]);
-    return 0;
+    std::string s1 = av[2];
+    std::string s2 = av[3];
+    inFile.open(av[1], std::ios::in);
+    if (!inFile.is_open())
+    {
+        std::cerr << "Error: inFile not open!\n";
+        return (1);
+    }
+    std::string outFileName = av[1];
+    outFile.open(outFileName + ".replace", std::ios::trunc | std::ios::out);
+    if (!outFile.is_open())
+    {
+        inFile.close();
+        std::cerr << "Error: outFile not open!\n";
+        return (1);
+    }
+    if (!s1[0] || !s2[0]) {
+        inFile.close();
+        outFile.close();
+        std::cerr << "Error: string1 and|or string2 is empty\n";
+        return (1);
+    }
+    FileReplace(inFile, outFile, s1, s2);
+    inFile.close();
+    outFile.close();
+    return (0);
 }
