2518e0fc50dae3fa0c1d53f9750bd9e754b3a418
BarBaRoSa1337 <achakour@1337.ma>
2025-06-19 16:27:15 +0100

    test

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,11 +1,10 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 #include <iostream>
-<<<<<<< HEAD
 #include <string>
 
-template <typename X>
-void iter(X* arr, int lenght, void (*fun)(const X& value))
+template <typename X, typename F>
+void iter(X* arr, int lenght, F value)
 {
     if (lenght <= 0 || !arr || !fun)
         return;
@@ -18,17 +17,3 @@ void iter(X* arr, int lenght, void (*fun)(const X& value))
 }
 
 #endif // ITER_HPP
-=======
-
-template <typename X, typename F>
-void iter(X* array, long lenght, F fun)
-{
-    long i = 0;
-    while (i < lenght)
-    {
-        fun(array[i]);
-        ++i;
-    }
-}
-#endif
->>>>>>> 1a5507ec1065ad9c905097cad70141ecd2d697a7
